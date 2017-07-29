f7c77d132cd78eca4d0375f8df884bfff8a4b02f
Andrei Constantinescu <andrei@constantinescu.io>
2017-07-29 19:06:50 +0100

    Add Homepage.

--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,40 @@
 import React from 'react'
 import Layout from '../components/layout.js'
 
-type props = {
-  children?: React$Element<*>
-}
-
-const B = ({ children }: props) =>
-  <strong>
-    {children}
-    <style jsx>{`
-      strong {
-        font-weight: 300;
-      }
-    `}</style>
-  </strong>
-
 export default () =>
   <Layout>
-    <h2>
-      <B>Andrei</B>Constantinescu
-    </h2>
     <ul>
-      <li>Email</li>
-      <li>CV</li>
-      <li>Github</li>
+      <li>
+        Email:{' '}
+        <a href='mailto:andrei@constantinescu.io'>andrei@constantinescu.io</a>
+      </li>
+      <li>
+        CV:{' '}
+        <a href='//andreiconstantinescu.github.io/cv'>constantinescu.io/cv</a>
+      </li>
+      <li>
+        Github:{' '}
+        <a href='//github.com/andreiconstantinescu'>
+          github.com/andreiconstantinescu
+        </a>
+      </li>
+      <li>
+        Linkedin:{' '}
+        <a href='//linkedin.com/in/constantinescuio'>
+          linkedin.com/in/constantinescuio
+        </a>
+      </li>
     </ul>
-
     <style jsx>{`
-      h2 {
-        font-weight: 100;
-      }
-
       ul {
-        display: flex;
-        justify-content: center;
         list-style-type: none;
         padding: 0;
+        font-size: 1.5rem;
+        font-weight: 300;
       }
 
-      ul li:not(:last-child):after {
-        content: '·';
-        margin: 0 0.5rem;
+      ul li {
+        margin: 1rem 0;
       }
     `}</style>
   </Layout>
