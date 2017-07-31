c24dc006de886608e68b148846edfdb7cd481437
Andrei Constantinescu <andrei@constantinescu.io>
2017-07-31 10:13:09 +0100

    WIP.

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,39 @@ import Layout from '../components/layout.js'
 
 export default () =>
   <Layout>
-    <ul>
-      <li>
-        CV: <a href='//andreiconstantinescu.github.io/cv'>constantinescu.io/cv</a>
-      </li>
-      <li>
-        Email: <a href='mailto:andrei@constantinescu.io'>andrei@constantinescu.io</a>
-      </li>
-      <li>
-        Github: <a href='//github.com/andreiconstantinescu'>github.com/andreiconstantinescu</a>
-      </li>
-    </ul>
+    <div>
+      <a href='//andreiconstantinescu.github.io/cv'>Resume</a>
+      <a href='mailto:andrei@constantinescu.io'>Email me</a>
+      <a href='//github.com/andreiconstantinescu'>Github</a>
+    </div>
     <style jsx>{`
-      ul {
-        list-style-type: none;
-        padding: 0;
-        font-weight: 300;
-        font-size: 1.2rem;
+      a {
+        padding: 0.5rem 1rem;
+        border: 1px solid #000;
+        text-decoration: none;
+        transition: background-color .15s ease-in-out;
+        color: inherit;
+        flex-basis: 2.5rem;
       }
-      ul li {
-        margin: 1rem 0;
+
+      a:hover,
+      a:focus {
+        color: #fff;
+        background-color: #000;
+      }
+
+      div {
+        display: flex;
+        flex-direction: column;
       }
 
       @media (min-width: 30rem) {
-        ul {
-          font-size: 1.5rem;
+
+        div {
+          text-align: center;
+          flex-basis: 5rem;
+          flex-direction: row;
+          justify-content space-between;
         }
       }
     `}</style>
