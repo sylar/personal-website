6417cadab2c4871919b0aad90bbf5f1d226eb24d
Andrei Constantinescu <andrei@constantinescu.io>
2017-08-05 21:44:42 +0100

    Add autogeneration of readme for submodules.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 // @flow
 // next.config.js
+const fs = require('fs')
+const Readme = require('./metadata/Readme.md.js')
 
 exports.exportPathMap = () => {
   // console.log(process.env.CURRENT_ITEM);
   // console.log(process.env.EXTERNAL_PATH);
-  console.log('hier', process.env.CURRENT_ITEM === 'home' ? '/' : `/${process.env.CURRENT_ITEM}`)
+  fs.writeFileSync(
+    `./out/${process.env.CURRENT_ITEM}/Readme.md`,
+    Readme({
+      repo: 'THE REPO YO',
+      deployUrl: 'https://constantinescu.io/cv'
+    })
+  )
+
   return {'/': {page: process.env.CURRENT_ITEM === 'home' ? '/' : `/${process.env.CURRENT_ITEM}`}}
 }
