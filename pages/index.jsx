95a115e2f43d89598a42b2a8f155d3fffbe157d4
Andrei Constantinescu <andrei@constantinescu.io>
2018-08-04 19:11:39 +0100

    Separate main container styles.

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,17 @@ import {homepageQuery} from '../lib/gql'
 const Homepage = ({data}) => {
   const {urlsBlock, logo, description} = data.homepage
   return (
-    <Layout customCss={{display: 'flex', flexDirection: 'column', boxShadow: '0px 0px 2px 0px rgba(0,0,0,0.15)'}}>
+    <Layout
+      customCss={{
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '0px 0px 2px 0px rgba(0,0,0,0.15)',
+        padding: '3rem',
+        paddingBottom: '1.5rem',
+        borderRadius: '10px',
+        backgroundColor: '#f5f5f5'
+      }}
+    >
       <Logo src={logo} />
       <HR />
       <Paragraph
