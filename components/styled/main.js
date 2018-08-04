ee7f7c4e7251b513214941256e628fa21722d552
Andrei Constantinescu <andrei@constantinescu.io>
2018-08-04 19:11:39 +0100

    Separate main container styles.

--- components/styled/main.js
+++ components/styled/main.js
@@ -3,10 +3,6 @@ import styled from 'react-emotion'
 const Main = styled('main')(({customCss}) => ({
   maxWidth: '25rem',
   margin: '4.5rem auto',
-  padding: '3rem',
-  paddingBottom: '1.5rem',
-  borderRadius: '10px',
-  backgroundColor: '#f5f5f5',
   ...customCss
 }))
 
