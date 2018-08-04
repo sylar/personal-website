ea0d97362974bdc6a854c5411ba8f13813746e56
Andrei Constantinescu <andrei@constantinescu.io>
2018-08-04 21:56:24 +0100

    WIP.

---
+++ components/sliceHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Heading} from './styled'
+
+const SliceHeader = ({md}) => <Heading md={md} />
+
+SliceHeader.displayName = 'SliceHeader'
+
+export default SliceHeader
--- pages/cv.jsx
+++ pages/cv.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import {graphql} from 'react-apollo'
-import {Layout} from '../components'
+import {Layout, Logo, SliceHeader} from '../components'
 import {cvQuery} from '../lib/gql'
+import {map} from 'lodash'
 
-const Homepage = ({data}) => {
-  const {urlsBlock, logo, title, description} = data.resume
+const CvPage = ({data}) => {
+  const {lastUpdate, title, email, slices, logo} = data.resume
+
+  console.log(slices)
   return (
-    <Layout>
+    <Layout
+      customCss={{
+        maxWidth: '50rem',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '3rem'
+      }}
+    >
+      <Logo src={logo} />
+      <SliceHeader md={slices.experience.title} />
     </Layout>
   )
 }
 
-export default graphql(homepageQuery)(Homepage)
+CvPage.displayName = 'CvPage'
 
-Homepage.displayName = 'Homepage'
+export default graphql(cvQuery)(CvPage)
