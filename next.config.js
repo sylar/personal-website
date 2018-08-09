723b967da75e7bf9ce3c65cbe5f6e80587030410
Andrei Constantinescu <andrei@constantinescu.io>
2018-08-09 23:08:06 +0100

    Add webpack targets.

--- next.config.js
+++ next.config.js
@@ -36,6 +36,17 @@ module.exports = {
   //   //   }
   //   // }
   // },
+  webpack: function (config, {dev}) {
+    if (!dev) {
+      const TargetsPlugin = require('targets-webpack-plugin')
+      config.plugins.push(
+        new TargetsPlugin({
+          browsers: ['last 2 versions', 'chrome >= 41']
+        })
+      )
+    }
+    return config
+  },
   serverRuntimeConfig: {
     PRISMIC_API_KEY:
       process.env.PRISMIC_API_KEY || (env ? env.PRISMIC_API_KEY : null),
