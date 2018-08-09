f8d1cb43d7347f409139646902e066cb21dd8cfc
Andrei Constantinescu <andrei@constantinescu.io>
2018-08-09 19:08:00 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -8,38 +8,38 @@ const getCorrectUrl = currentRepo =>
   currentRepo === 'andreiconstantinescu.github.io' ? '' : currentRepo
 
 const isDev = process.env.NODE_ENV !== 'production'
-let env = {}
+let env = null
 
-if (isDev) {
-  env = require('./env.json')
-}
+// if (isDev) {
+// env = require('./env.json')
+// }
 
 module.exports = {
-  exportPathMap: () => {
-    if (isDev) {
-      return
-    }
-
-    fs.writeFileSync(
-      `./out/${process.env.CURRENT_PAGE}/Readme.md`,
-      generateReadme({
-        repo: process.env.CURRENT_REPO,
-        deployUrl: `https://constantinescu.io/${getCorrectUrl(
-          process.env.CURRENT_REPO
-        )}`
-      })
-    )
-
-    return {
-      '/': {
-        page: getCorrectPage(process.env.CURRENT_PAGE)
-      }
-    }
-  },
+  // exportPathMap: () => {
+  //   if (isDev) {
+  //
+  //   }
+  //
+  //   // fs.writeFileSync(
+  //   //   `./out/${process.env.CURRENT_PAGE}/Readme.md`,
+  //   //   generateReadme({
+  //   //     repo: process.env.CURRENT_REPO,
+  //   //     deployUrl: `https://constantinescu.io/${getCorrectUrl(
+  //   //       process.env.CURRENT_REPO
+  //   //     )}`
+  //   //   })
+  //   // )
+  //   //
+  //   // return {
+  //   //   '/': {
+  //   //     page: getCorrectPage(process.env.CURRENT_PAGE)
+  //   //   }
+  //   // }
+  // },
   serverRuntimeConfig: {
     PRISMIC_API_KEY:
-      process.env.PRISMIC_API_KEY || env ? env.PRISMIC_API_KEY : null,
-    PRISMIC_API: process.env.PRISMIC_API || env ? env.PRISMIC_API : null
+      process.env.PRISMIC_API_KEY || (env ? env.PRISMIC_API_KEY : null),
+    PRISMIC_API: process.env.PRISMIC_API || (env ? env.PRISMIC_API : null)
   },
   publicRuntimeConfig: {
     GRAPHQL_ENDPOINT:
--- server/prismic/Fetcher.js
+++ server/prismic/Fetcher.js
@@ -1,11 +1,6 @@
 const Prismic = require('prismic-javascript')
 const getConfig = require('next/config').default
 
-// const {
-//   serverRuntimeConfig: {PRISMIC_API_KEY},
-//   publicRuntimeConfig: {PRISMIC_API}
-// } = getConfig()
-
 const getPagePayload = async ({page, pageType, query, handler}) => {
   const {
     serverRuntimeConfig: {PRISMIC_API_KEY, PRISMIC_API}
@@ -14,6 +9,8 @@ const getPagePayload = async ({page, pageType, query, handler}) => {
   const api = await Prismic.api(PRISMIC_API, {
     accessToken: PRISMIC_API_KEY
   })
+
+  console.log({api, PRISMIC_API, PRISMIC_API_KEY})
   // Prismic's fault :(
   // eslint-disable-next-line
   const {data, last_publication_date} = await api.getByUID(pageType, page, {
