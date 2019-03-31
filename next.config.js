3cf8f9703c8481a3d73e585e1994e1742648f23c
Andrei Constantinescu <andrei@constantinescu.io>
2019-03-31 15:26:46 +0100

    Use dotenv in next-config.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,27 @@
+require('dotenv').config()
+
+const { GRAPHQL_ENDPOINT, NODE_ENV, PORT } = process.env
+
 module.exports = {
   webpack: function (config, { dev }) {
+    config.plugins = config.plugins || []
+
     if (!dev) {
       const TargetsPlugin = require('targets-webpack-plugin')
-      config.plugins.unshift(
+      config.plugins = [
+        ...config.plugins,
         new TargetsPlugin({
           browsers: ['last 2 versions', 'chrome >= 41', 'ie >= 11']
         })
-      )
+      ]
     }
+
     return config
   },
+
   publicRuntimeConfig: {
-    GRAPHQL_ENDPOINT:
-      process.env.GRAPHQL_ENDPOINT || 'http://localhost:7777/graphql'
+    GRAPHQL_ENDPOINT: GRAPHQL_ENDPOINT,
+    NODE_ENV: NODE_ENV,
+    PORT: PORT
   }
 }
