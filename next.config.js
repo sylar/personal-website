03668e3358581eacc2c9065f6604af630f09634b
Andrei Constantinescu <andrei@constantinescu.io>
2019-04-15 22:26:10 +0100

    Add gql endpoint to now.

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ if (NODE_ENV === 'development') {
   require('dotenv').config()
 }
 
-const { GRAPHQL_ENDPOINT, PORT } = process.env
+const { GRAPHQL_ENDPOINT } = process.env
 module.exports = {
+  env: {
+    GRAPHQL_ENDPOINT
+  },
   target: 'serverless',
   exportPathMap: async function() {
     return {
