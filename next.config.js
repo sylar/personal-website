be5cb153b1e7b8439d89fa0b6006e75f567c7253
Andrei Constantinescu <andrei@constantinescu.io>
2019-04-15 21:58:11 +0100

    Remove corejs and target browsers.

--- next.config.js
+++ next.config.js
@@ -5,22 +5,6 @@ if (NODE_ENV === 'development') {
 
 const { GRAPHQL_ENDPOINT, PORT } = process.env
 module.exports = {
-  webpack: function(config, { dev }) {
-    config.plugins = config.plugins || []
-
-    if (!dev) {
-      const TargetsPlugin = require('targets-webpack-plugin')
-      config.plugins = [
-        ...config.plugins,
-        new TargetsPlugin({
-          browsers: ['last 2 versions', 'chrome >= 41', 'ie >= 11']
-        })
-      ]
-    }
-
-    return config
-  },
-
   publicRuntimeConfig: {
     GRAPHQL_ENDPOINT: GRAPHQL_ENDPOINT,
     NODE_ENV: NODE_ENV,
