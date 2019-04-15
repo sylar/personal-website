58ba33baaa6845baec5e49189ca4998d1f2582ae
Andrei Constantinescu <andrei@constantinescu.io>
2019-04-15 22:07:00 +0100

    nend

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ if (NODE_ENV === 'development') {
 const { GRAPHQL_ENDPOINT, PORT } = process.env
 module.exports = {
   target: 'serverless',
-  publicRuntimeConfig: {
-    GRAPHQL_ENDPOINT: GRAPHQL_ENDPOINT,
-    NODE_ENV: NODE_ENV,
-    PORT: PORT
-  },
-
   exportPathMap: async function() {
     return {
       '/': { page: '/' },
