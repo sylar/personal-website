bba73218a862a1fa990ec08097138aeadd0d65f0
Andrei Constantinescu <andrei@constantinescu.io>
2020-06-03 21:28:35 +0100

    Update data files.

---
+++ data/projects.ts
@@ -0,0 +1,25 @@
+export default [
+  {
+    name: "dotfiles",
+    repo: "https://github.com/sylar/dorfiles",
+    details: "My personal dotfiles that I consistantly update."
+  },
+  {
+    name: "world-capitals",
+    repo: "https://github.com/sylar/world-capital",
+    details:
+      "An Alexa trivia game in which you are challenged to guess  10 random capital cities."
+  },
+  {
+    name: "personal-website",
+    repo: "https://github.com/sylar/personal-website",
+    details:
+      "My personal website build with reactjsm nextjs and deployed with nowsh."
+  },
+  {
+    name: "wordpress-jailcell",
+    repo: "https://github.com/sylar/wordpress-jailcell",
+    details:
+      "A docker based development environment for developing with wordpress."
+  }
+];
