c8a954a25512f8f2408d0573c09b699bc09cfb5b
Andrei Constantinescu <andrei@constantinescu.io>
2020-06-03 21:28:35 +0100

    Update data files.

---
+++ data/details.ts
@@ -0,0 +1,25 @@
+export default {
+  links: [
+    {
+      label: "email",
+      url: "mailto:andrei@constantinescu.io"
+    },
+    {
+      label: "github",
+      url: "https://github.com/sylar"
+    },
+    {
+      label: "linkedIn",
+      url: "https://linkedin.com/constantinescuio"
+    },
+    {
+      label: "resume",
+      url: "/cv",
+      prefetch: true
+    }
+  ],
+  descripion:
+    "I am a Senior Software Engineer with an eye for javascript, usability and performance that tries to be up to date and enjoys a workout with every occasion",
+  hobbies:
+    "Mostly I read biographies and nonfiction books as well as tech news from hacker news. In my spare time I enjoy going to the gym and also practice playing  the guitar, should I have the time."
+};
