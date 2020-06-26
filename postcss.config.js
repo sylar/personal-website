6adf8dc0fb27cdcaf5d7773e4f950d5978eeb5e5
Andrei Constantinescu <andrei@constantinescu.io>
2020-06-27 00:20:47 +0100

    Update tailwindcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,3 @@
-const { NODE_ENV } = process.env
-const isProd = NODE_ENV === 'production'
-
-const plugins = [
-  'tailwindcss',
-  'postcss-preset-env',
-  ...(isProd
-    ? [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        },
-      ]
-    : []),
-  'postcss-preset-env',
-]
 module.exports = {
-  plugins,
+  plugins: ['tailwindcss', 'postcss-preset-env']
 }
