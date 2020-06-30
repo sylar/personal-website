11f3fb3c10f01f1d09da89b00734ac181acf3d37
Andrei Constantinescu <andrei@constantinescu.io>
2020-07-01 00:02:36 +0100

    Weird syntax for prettier.

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,10 +1,10 @@
-import * as styled from 'styled-components'
+import { createGlobalStyle, css, DefaultTheme } from 'styled-components'
 
 const COLORS = {
   electromagnetic: '#2f3640',
   lynxWhite: '#f5f6fa'
 }
-const Theme: styled.DefaultTheme = {
+const Theme: DefaultTheme = {
   colors: {
     primary: COLORS.electromagnetic,
     secondary: COLORS.lynxWhite
@@ -18,12 +18,13 @@ const Theme: styled.DefaultTheme = {
     '-apple-system, BlinkMacSystemFont,  BlinkMacSystemFont,  BlinkMacSystemFont,  BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
 }
 
-export const GlobalStyle = styled.createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`${css`
   body,
   html {
     font-family: ${(props) => props.theme.fontFamily};
     background: ${(props) => props.theme.background.light};
   }
+`}
 `
 
 export default Theme
