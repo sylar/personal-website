2ae0069fd88a9d8d2fa11a131189627dec059a0f
Andrei Constantinescu <andrei@constantinescu.io>
2020-07-01 23:41:35 +0100

    Add _app pagetitle.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,23 @@ import App from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { PageLayout } from '../components/styled'
 import theme, { GlobalStyle } from '../theme'
+import Head from 'next/head'
 
 class MyApp extends App {
   render(): JSX.Element {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-
-        <PageLayout>
-          <Component {...pageProps} />
-        </PageLayout>
-      </ThemeProvider>
+      <>
+        <Head>
+          <title>Andrei Constantinescu</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <PageLayout>
+            <Component {...pageProps} />
+          </PageLayout>
+        </ThemeProvider>
+      </>
     )
   }
 }
