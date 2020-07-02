bafa6f16a4edb663d97917ecdb028b5ad6b8b647
Andrei Constantinescu <andrei@constantinescu.io>
2020-07-02 20:59:11 +0100

    Add control links on  resume page.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,33 @@
 import { Header, HeaderTitle } from './styled'
 import Logo from '../Logo'
 import { HeaderProps } from './types'
+import LinkList from '../Link/LinkList'
+import { Div, Small, Sizes } from '../../lib/styles/global'
+import { LinkProps } from '../Link/types'
+
+const links: LinkProps[] = [
+  {
+    label: 'Home',
+    url: '/',
+    internal: true
+  },
+  {
+    label: 'Email',
+    url: 'mailto:andrei@constantinescu.io'
+  }
+]
 
 const HeaderComponent = ({ size }: HeaderProps): JSX.Element => (
   <Header size={size}>
-    <HeaderTitle size={size}>
-      Andrei <br />
-      Constantinescu
-    </HeaderTitle>
     <Logo size={size} />
+    <Div>
+      <HeaderTitle size={size}>Andrei Constantinescu</HeaderTitle>
+      {size === Sizes.SMALL && (
+        <Small>
+          <LinkList urls={links} />
+        </Small>
+      )}
+    </Div>
   </Header>
 )
 
--- src/components/Logo/styled.tsx
+++ src/components/Logo/styled.tsx
@@ -3,19 +3,22 @@ import { Image, Section, Sizes } from '../../lib/styles/global'
 import { LogoProps } from './types'
 
 export const LogoContainer = styled(Section)`
-  width: 100%;
-
   ${(props: Partial<LogoProps>) => {
     switch (props.size) {
       case Sizes.SMALL:
         return css`
-          width: ${(props) => props.theme.typography.lineHeightSpacing(3)};
-          height: ${(props) => props.theme.typography.lineHeightSpacing(3)};
+          svg {
+            width: ${(props) => props.theme.typography.lineHeightSpacing(4.5)};
+            height: ${(props) => props.theme.typography.lineHeightSpacing(4.5)};
+          }
         `
       case Sizes.MEDIUM:
         return css`
-          width: ${(props) => props.theme.typography.lineHeightSpacing(6)};
-          height: ${(props) => props.theme.typography.lineHeightSpacing(6)};
+          text-align: center;
+          svg {
+            width: ${(props) => props.theme.typography.lineHeightSpacing(6)};
+            height: ${(props) => props.theme.typography.lineHeightSpacing(6)};
+          }
         `
       default:
         return css`
