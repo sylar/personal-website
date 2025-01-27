259ac88ad01d4a753d48a103d36e09864eb8bad8
Andrei Constantinescu <andrei@constantinescu.io>
2025-01-27 22:30:38 +0000

    Add gh action

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -6,10 +6,6 @@ set -e
 # Log the start of the post-deployment script
 echo "Running post-deployment script..."
 
-# Print environment information
-echo "Deployment Environment: ${VERCEL_ENV}"
-echo "Deployment URL: ${VERCEL_URL}"
-
 # Run your specific post-deployment command
 # Example: Generate PDF
 npm run resume:generate
